f6cff7eb24c9b2ac9ca7899c3c4521761b509243
jenna-P <jenn2464@edu.easj.dk>
2020-10-27 16:21:46 +0100

    Initial commit

---
+++ src/Uge41/MathDemo.java
@@ -0,0 +1,18 @@
+package Uge41_man;
+
+public class MathDemo {
+    public static void main(String[] args) {
+        Math.toDegrees(Math.PI);
+        Math.sin(90.0);
+        Math.cos(180.0);
+        Math.asin(0.5);
+        double pi = Math.PI;
+
+
+        System.out.println(Math.toDegrees(Math.cos(180.0)));
+        System.out.println(Math.asin(0.5));
+        System.out.println(pi);
+        System.out.println(Math.E);
+    }
+
+}
---
+++ src/Uge44/ColourInArray.java
@@ -0,0 +1,66 @@
+package Uge44;
+/*  Lav en array med X  tilfældige farvenavne (X skal være konstant)
+    Lav en array med Y tilfældige farver (Y skal være konstant)
+    Se om farven Z kan findes i Y (Z skal også være konstant)
+    Ekstra: løs opgaven på 3 forskellige måder: først skal farvene repræsenteres af Strings,
+    herefter skal de repræsenteres med klassen Colors,
+    til sidst skal de repræsenteres af en enum med navnet Colortypes.
+    (Hint: se på enums .values() metode, som returnerer en array)
+    Ekstra: omskriv opgaven, så farvearray med Y elementer bliver til en ArrayList i stedet for en array.*/
+
+public class ColourInArray {
+
+    public static void main(String[] args) {
+
+        final String[] X = new String[7];
+        X[0] = "Red";
+        X[1] = "Blue";
+        X[2] = "White";
+        X[3] = "Black";
+        X[4] = "Green";
+        X[5] = "Yellow";
+        X[6] = "Brown";
+
+        final String[] Y = {X[0], X[3], X[5]};
+        final String Z = "Black";
+
+
+        for (String c : Y) {
+            if (c.equals(Z))
+                System.out.println(Z);
+
+        }
+        new ColourInArray().callColourMethod();
+        EnumColour.callenum();
+
+    }
+
+
+
+    public class Colour {
+        String[] colours = {"Red", "Blue", "White", "Black", "Green", "Yellow", "Brown"};
+
+    }
+    public void callColourMethod() {
+        Colour colour = new Colour();
+        String R = colour.colours[0];
+        System.out.println(R);
+    }
+    
+    public enum EnumColour {
+       Red, Blue, White, Black, Green, Yellow, Brown ;
+
+        static void callenum() {
+            System.out.println("One colour in a enumclass : " + EnumColour.valueOf("White"));
+
+        }
+
+    }
+
+
+
+}
+
+
+
+
